aaf11b38512a9a6b1e8be91aa2525bbf04ed826c
Rajarajan Abathsagayam <rajarajan.abathsagayam@gmail.com>
2025-02-04 20:50:01 +0530

    Successfully Integrated NativeWind with the React Native Expo

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import "./global.css";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View className="flex-1 justify-center items-center bg-blue-400">
+      <View className="bg-white p-10 rounded-lg shadow-lg m-5 border-red-500 border-4">
+        <Text className="text-red-500">
+          Open up App.tsx to start working on your app!
+        </Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +18,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
